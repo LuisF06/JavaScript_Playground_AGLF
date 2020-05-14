810028add028376f79da76a893c905ec6d613fb2
Luis Fernando Alvarez Guerrero <luisfalvarezguerrero@gmail.com>
2020-05-14 11:15:29 -0500

    Update HelloWorld.js

--- Lesson 01/HelloWorld.js
+++ Lesson 01/HelloWorld.js
@@ -1,1 +1,4 @@
-console.log("Hello World!\nHola Mundo!\nCiao Mundo!")
+let msg = "Hello World!\nHola Mundo!\nCiao Mundo!";
+var msg2 = "Another way to create a variable";
+console.log(msg)
+console.log(msg2)
